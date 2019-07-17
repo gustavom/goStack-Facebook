23e1653dee05816570226d3a0446b2e62a85b381
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-17 17:51:41 -0300

    post list base markup

--- src/components/postList/PostList.js
+++ src/components/postList/PostList.js
@@ -1,3 +1,15 @@
-import React from 'react';
+import React, { Component} from 'react';
 
 import './_post-list.scss';
+
+class PostList extends Component{
+  render(){
+    return (
+      <main className="post-list">
+        <div className="post">Olá</div>
+      </main>
+    )
+  }
+}
+
+export default PostList;
