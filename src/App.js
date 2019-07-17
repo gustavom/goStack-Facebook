3e21fed436754c8712f95325f1c9db02df27729c
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-17 17:51:46 -0300

    import post list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './assets/scss/styles.scss';
 import Header from './components/header/Header'
+import PostList from './components/postList/PostList'
 
 function App(){
-  return <Header />
-}
+  return (
+      <>
+        <Header />
+        <PostList />
+      </>
+    );
+};
 
 export default App;
