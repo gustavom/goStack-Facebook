8069b4bfeff0616187395c968ce697b9627d705f
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-17 19:04:06 -0300

    post item component

--- src/components/postItem/PostItem.js
+++ src/components/postItem/PostItem.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
+import './_post-item.scss';
+
 function PostItem({id, name, avatar, date, content,comments}){
   return(
-    <div className="post-item">
-      {id}
-      {name},
-      <img src={avatar}/>
-      {date},
-      {content}
+    <div className={`post-item ${id}`}>
+      <div className="post-header">
+        <span className="image">
+          <img src={avatar}/>
+        </span>
+        <div className="author-infos">
+          <h2>{name}</h2>
+          <span>{date}</span>
+        </div>
+      </div>
+      <div className="post-text">
+        <p>
+        {content}
+        </p>
+      </div>
+      
+      
+      
+      
     </div>
   );
 }
