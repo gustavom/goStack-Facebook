4c388b496dd5bfeca9cd4e9f694afbf84b88a595
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-18 10:38:17 -0300

    change project logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './_header.scss';
 import Facebook from '../../assets/image/facebook.svg';
+import GoBook from '../../assets/image/goBook.svg';
 import UserIcon from '../../assets/image/icon-user.svg';
 
 
 function Header(){
   return(
     <header className="header">
-      <Facebook width="121px" />
+      <GoBook width="121px"/>
       <div className="profile">
         <span>Meu perfil</span>
       <UserIcon fill="#ffffff" width="25" />
