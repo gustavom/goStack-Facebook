a614bcf0c3468e5bc55cba1774387b0ef5edb74d
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-17 22:08:31 -0300

    finish comment component

--- src/components/commentItem/CommentItem.js
+++ src/components/commentItem/CommentItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import './_comment-item.scss';
+
 function CommentItem({name, avatar, content}){
   return(
     <div className="comment-item">
       <div className="image">
         <img src={avatar}/>
+      </div>
+      <div className="content">
+        <h3>{name}</h3>
+        <p>{content}</p>
       </div>
     </div>
   )
