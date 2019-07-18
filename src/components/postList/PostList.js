a2bcbc63955d8fd18ac86f5dfb1501dd9cd0e7ae
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-17 22:08:08 -0300

    add new comments

--- src/components/postList/PostList.js
+++ src/components/postList/PostList.js
@@ -39,10 +39,18 @@ class PostList extends Component{
           {
             id: 1,
             author: {
-              name: 'Diego Fernandes',
-              avatar: 'http://url-da-imagem.com/imagem.jpg'
+              name: 'Clara Lisboa',
+              avatar: 'users/user-04.png'
             },
-            content: "A Rocketseat está sempre em busca de novos membros para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, pode me chamar no Discord! (Sério, me chamem mesmo, esse comentário é real)."
+            content: "Também estou fazendo o Bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha API dos desafios contruída!"
+          },
+          {
+            id: 2,
+            author: {
+              name: 'Cézar Toledo',
+              avatar: 'users/user-05.png'
+            },
+            content: "Que maaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes!"
           }
         ],
       },
